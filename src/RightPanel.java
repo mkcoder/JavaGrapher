14aaeeecfbcb9641362daf5c3a29a522b2d8e279
MaciejSzpakowski <systemzajety@yahoo.com>
2015-11-21 17:11:23 -0600

    Right panel preferred size

--- src/RightPanel.java
+++ src/RightPanel.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class RightPanel extends JPanel
@@ -5,7 +8,8 @@ public class RightPanel extends JPanel
 	private DrawManager drawManager;
 	
 	public RightPanel(DrawManager drawManager)
-	{
+	{		
 		this.drawManager = drawManager;
+		setPreferredSize(new Dimension(200,1000));
 	}
 }
