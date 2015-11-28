280aa5ff33dd19ad4163426d69d205c169e5e36e
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2015-11-28 14:26:57 -0600

    made changes to right panel  UI
    right panel now has working header for the table,
    update button.

--- RightPanel.java
+++ RightPanel.java
@@ -14,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.event.CellEditorListener;
@@ -94,21 +95,25 @@ public class RightPanel extends JPanel
 			dialog.setPreferredSize(new Dimension(350, 350));
 			dialog.setResizable(false);			
 			dialog.setTitle("Options for " + table.getModel().getValueAt(row, column-1));
-			
-			options_value.add(new JLabel("Function: "));
 			function = new JTextField(25);
 			function.setText(table.getModel().getValueAt(row, column-1).toString());
-			options_value.add(function);
-			
-			options_value.add(new JLabel("Color: "), BorderLayout.EAST);			
 			color_button = new JButton("Change color");
-			
+
+			// event handlers
 			update.addActionListener(new ActionListener() {
-				
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					// TODO Auto-generated method stub
-					dm.paintComponent(getGraphics());
+					int index = new Integer(table.getModel()
+							.getValueAt(row, column-2).toString());
+					String expression = function.getText(); 
+							
+							table.getModel()
+							.setValueAt(expression, row, column-1);
+					boolean visible = true;
+					Function f = dm.getFunction(index);
+					f.setExpression(expression);
+					f.setVisible(visible);
 				}
 			});
 			
@@ -123,8 +128,15 @@ public class RightPanel extends JPanel
 							.showDialog(dialog, "Choose a color", userColor));					
 				}
 			});
-			options_value.add(color_button);
-			options_value.add(update, BorderLayout.SOUTH);			
+			
+
+			options_value.add(new JLabel("Function: "));
+			options_value.add(function);
+			options_value.add(new JLabel("Color: "), BorderLayout.EAST);		
+			options_value.add(color_button, BorderLayout.WEST);
+			options_value.add(update, BorderLayout.PAGE_START);	
+			
+			dialog.getContentPane().setLayout(new BorderLayout());
 			dialog.getContentPane().add(options_value);			
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);	
 			
@@ -134,15 +146,10 @@ public class RightPanel extends JPanel
 		}
 
 	}
-
-	String [] tableColumns = {
-		"Equation number", 
-		"Equation",
-		"Options"
-	};
 	
-	private JPanel function_panel;
-	private JPanel panel;	
+	private JPanel functionInputPanel;
+	private JPanel tableHolderPanel;
+	
 	private DefaultTableModel dtm;
 	private JButton add_function;
 	protected JTextField lineFunction;
@@ -152,22 +159,32 @@ public class RightPanel extends JPanel
 	{		
 
 		final DrawManager drawManager = _drawManager;
+		JScrollPane tablePane = new JScrollPane(table);
+		JFrame tableFrame = new JFrame();
 		
-		panel = new JPanel();
-		function_panel = new JPanel();		
-		dtm = new DefaultTableModel(new String[] {
+		functionInputPanel = new JPanel();		
+		tableHolderPanel = new JPanel();
+		
+		dtm = new DefaultTableModel(new Object[] {
 				"Equation number", 
 				"Equation",
 				"Options"
-			}, 0);		
+			}, 0);				
 		add_function = new JButton("Add");
 		lineFunction = new JTextField(15);			
 		lineFunction.setEnabled(true);
-		table = new JTable();
+		table = new JTable(null, new String[] {
+				"Equation number", 
+				"Equation",
+				"Options"
+			});
 		
-		dtm.setColumnIdentifiers(tableColumns);
 		table.setModel(dtm);	
-		
+		dtm.setColumnIdentifiers(new Object[] {
+				"INDEX", 
+				"EXPRESSION",
+				"OPTION"
+			});
 		
 		// add event handlers here
 		
@@ -182,14 +199,19 @@ public class RightPanel extends JPanel
 			}
 		});
 		
-		function_panel.add(new JLabel("Y="), BorderLayout.EAST);
-		function_panel.add(lineFunction, BorderLayout.EAST);
-		function_panel.add(add_function, BorderLayout.WEST);
-		add(function_panel,BorderLayout.WEST);
 		
-		add(table, BorderLayout.WEST);
-		table.getColumn("Options").setCellEditor(new OptionEditor(drawManager));
+		functionInputPanel.add(new JLabel("Y="), BorderLayout.PAGE_START);
+		functionInputPanel.add(lineFunction, BorderLayout.NORTH);
+		functionInputPanel.add(add_function, BorderLayout.NORTH);
+		table.getColumn("OPTION").setCellEditor(new OptionEditor(drawManager));
+
+		// create a table frame with the headers on top and the table on the center
+		tableHolderPanel.setLayout(new BorderLayout());
+		tableHolderPanel.add(table.getTableHeader(), BorderLayout.NORTH);
+		tableHolderPanel.add(table, BorderLayout.CENTER);
 		
+		add(functionInputPanel);
+		add(tableHolderPanel);
 		setPreferredSize(new Dimension(340,1000));
 	}
 		
