374481a25d08f4c986bd8896a976528e7554e961
MaciejSzpakowski <systemzajety@yahoo.com>
2015-12-02 00:39:29 -0600

    Parallel

--- Function.java
+++ Function.java
@@ -1,15 +1,15 @@
 import java.awt.*;
-import java.awt.geom.*;
-
-import javax.swing.JOptionPane;
 
 public class Function
 {
+	public static final int THREAD_COUNT = 2;
+	
 	private String expression;
 	private Color color;
 	private boolean visible;
 	private boolean showParticlesFlag;
 	private ParticleGenerator particles;
+	private Thread[] threads;	
 	
 	public Function(String expr, Color color, DrawManager d)
 	{
@@ -21,44 +21,65 @@ public class Function
 		particles = new ParticleGenerator(expression, compl, d);
 		visible = true;
 		showParticlesFlag = true;
+		threads = new Thread[THREAD_COUNT];
 	}
 	
 	public void draw(Graphics g, DrawManager d)
 	{
-		Point p1;
-		Point p2;
-		double x;
-		double y1;
-		double y2;
-		
-		if(!visible) // draw on not draw
+		for(int i=0;i<THREAD_COUNT;i++)
 		{
-			return;
-		}
-		
-		p1 = new Point();
-		p2 = new Point();
-		
-		for(int i = 0; i < d.getSize().width - 1; i++)
-		{			
-			x = d.screenToGlobalX(i);
-			y1 = Parser.evaluate(expression.replace("x", x + ""));
+			threads[i] = new Thread()
+			{				
+				public void run()
+				{
+					Point p1;
+					Point p2;
+					double x;
+					double y1;
+					double y2;
+					
+					if(!visible) // draw on not draw
+					{
+						return;
+					}
+					
+					p1 = new Point();
+					p2 = new Point();
+					
+					for(int i = 0; i < d.getSize().width - 1; i++)
+					{			
+						x = d.screenToGlobalX(i);
+						y1 = Parser.evaluate(expression.replace("x", x + ""));
+									
+						p1.x = i;
+						p1.y = d.globalToScreenY(-y1);                                /////////////////// WEIRD, WHY THIS HAS TO BE NEGATIVE !!!!
 						
-			p1.x = i;
-			p1.y = d.globalToScreenY(-y1);                                /////////////////// WEIRD, WHY THIS HAS TO BE NEGATIVE !!!!
-			
-			x = d.screenToGlobalX(i + 1);
-			y2 = Parser.evaluate(expression.replace("x", x + ""));
-			
-			if(!Double.isFinite(y1) || !Double.isFinite(y2)) // ignore infinities
+						x = d.screenToGlobalX(i + 1);
+						y2 = Parser.evaluate(expression.replace("x", x + ""));
+						
+						if(!Double.isFinite(y1) || !Double.isFinite(y2)) // ignore infinities
+						{
+							continue;
+						}
+						
+						p2.x = i+1;
+						p2.y = d.globalToScreenY(-y2);                                /////////////////// WEIRD, WHY THIS HAS TO BE NEGATIVE !!!!
+						
+						Brush.drawLine(g, p1, p2, color, 1);
+					}
+				}
+			};
+			threads[i].start();
+		}
+		for(Thread t : threads)
+		{
+			try
 			{
-				continue;
-			}
-			
-			p2.x = i+1;
-			p2.y = d.globalToScreenY(-y2);                                /////////////////// WEIRD, WHY THIS HAS TO BE NEGATIVE !!!!
-			
-			Brush.drawLine(g, p1, p2, color, 1);
+	            t.join();
+            }
+			catch (InterruptedException e) 
+			{
+            }
 		}
 		
 		if(showParticlesFlag)
